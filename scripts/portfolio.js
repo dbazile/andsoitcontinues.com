82aed7d4b31218c12c10270544826b31135593e5
David Bazile <davidbazile@gmail.com>
2016-10-10 15:27:56 -0400

    Fix various portfolio navigation quirks
    - Scroll-bouncing when moving between portfolio items
    - Having no visible element when entering the portfolio via a history.pushState page

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -13,35 +13,53 @@
   //
 
   function initialize() {
+    console.debug('(portfolio:initialize) subscribing to hashchange events')
     onHashChange()
     window.addEventListener('hashchange', onHashChange)
     asic.transitioner.beforeNext(teardown)
   }
 
   function onHashChange() {
-    console.debug('(portfolio:onHashChange):', location.hash)
-    var container = document.querySelector('.tiles')
+    // Update the tiles
+    var tiles = document.querySelector('.tiles')
     if (location.hash) {
-      container.classList.add('tiles--hasActiveTile')
+      tiles.classList.add('tiles--hasActiveTile')
     }
     else {
-      container.classList.remove('tiles--hasActiveTile')
+      tiles.classList.remove('tiles--hasActiveTile')
     }
-    var tiles = atoa(document.querySelectorAll('.tiles__tile'))
-    tiles.forEach(function (tile) {
-      if (tile.querySelector('a').hash === location.hash) {
-        tile.classList.add('tiles__tile--isActive')
-        var y = document.querySelector('.ui').offsetTop
-        window.scrollTo(0, y)
-      }
-      else {
-        tile.classList.remove('tiles__tile--isActive')
-      }
-    })
+    atoa(document.querySelectorAll('.tiles__tile'))
+      .forEach(function (element) {
+        if (element.querySelector('a').hash === location.hash) {
+          element.classList.add('tiles__tile--isActive')
+        }
+        else {
+          element.classList.remove('tiles__tile--isActive')
+        }
+      })
+
+    /*
+      This is needed because the :target CSS pseudoselector apparently doesn't
+      recompute if entering a hashed URL from a history.pushState'd URL.  This
+      manifests itself when moving _away_ from portfolio to another route, then
+      going back in history.
+
+      Refer to https://bugs.webkit.org/show_bug.cgi?id=83490
+     */
+    // Update the groups
+    atoa(document.querySelectorAll('.narrativeGroup'))
+      .forEach(function (element) {
+        if (element.id === location.hash.substr(1)) {
+          element.classList.add('narrativeGroup--isActive')
+        }
+        else {
+          element.classList.remove('narrativeGroup--isActive')
+        }
+      })
   }
 
   function teardown() {
-    console.debug('(portfolio:*) unsubscribing from hash change events')
+    console.debug('(portfolio:teardown) unsubscribing from hash change events')
     window.removeEventListener('hashchange', onHashChange)
   }
 }(window.asic))
