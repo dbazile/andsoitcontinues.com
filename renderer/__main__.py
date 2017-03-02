fc0feac0a89ed88370c1e82d2e5960af6b0aaf09
David Bazile <dbazile@users.noreply.github.com>
2017-03-02 00:40:06 -0500

    Reorganize folder structure

--- renderer/__main__.py
+++ renderer/__main__.py
@@ -5,9 +5,10 @@ from os.path import dirname, join
 from jinja2 import Environment, FileSystemLoader
 from renderer import blog, partials, portfolio
 
-ROOT = dirname(dirname(__file__))
+ROOT         = dirname(dirname(__file__))
+WEB_ROOT     = join(ROOT, 'web')
 MARKDOWN_DIR = join(ROOT, 'data/markdown')
-XML_DIR = join(ROOT, 'data/xml')
+XML_DIR      = join(ROOT, 'data/xml')
 TEMPLATE_DIR = join(ROOT, 'renderer/templates')
 
 logging.basicConfig(
@@ -22,6 +23,6 @@ env = Environment(
 
 env.filters['format_datetime'] = datetime.strftime
 
-blog.render(env, MARKDOWN_DIR, ROOT)
-portfolio.render(env, XML_DIR, ROOT)
-partials.render(env, ROOT)
+blog.render(env, MARKDOWN_DIR, WEB_ROOT)
+portfolio.render(env, XML_DIR, WEB_ROOT)
+partials.render(env, WEB_ROOT)
